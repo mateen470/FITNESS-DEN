05ed16ef824ea58b02ecd0f335c0c4bd7afea7af
Abdul Mateen <100369328+mateen470@users.noreply.github.com>
2023-03-30 16:53:59 +0500

    email sending function done

--- server/email/sendemail.js
+++ server/email/sendemail.js
@@ -1,1 +1,25 @@
 import nodemailer from "nodemailer";
+
+const sendEmail = (payload) => {
+  const emailCarrier = nodemailer.createTransport({
+    service: process.env.EMAIL_SERVICE,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  const mailPayload = {
+    from: process.env.EMAIL_FROM,
+    to: payload.to,
+    subject: payload.subject,
+    html: paylaod.text,
+  };
+
+  emailCarrier.sendMail(mailPayload, function (err, info) {
+    if (err) console.log(err);
+    console.log("EMAIL SENT SUCCESSFULLY!!", info);
+  });
+};
+
+module.exports = sendEmail;
