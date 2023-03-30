a8a315b6c19a5ad3c88705969591760526e973f0
Abdul Mateen <100369328+mateen470@users.noreply.github.com>
2023-03-30 15:08:34 +0500

    backend server file setup

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json("SERVER STARTED");
+});
+
+app.listen(PORT, () => {
+  console.log(`APP IS RUNNING ON http://localhost:${PORT}`);
+});
