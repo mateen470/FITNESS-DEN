c9662e749e3d36ef8338ec0ede8c47900ce35b09
Abdul Mateen <100369328+mateen470@users.noreply.github.com>
2023-03-31 19:44:43 +0500

    .

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
-import("./database/connection.js")
+import("./database/connection.js");
+import router from "./router/authroutes.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
+app.use("/fitness-den", router);
 
 app.get("/", (req, res) => {
   res.json("SERVER STARTED");
