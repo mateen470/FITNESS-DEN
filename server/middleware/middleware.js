7e4e2f447454d5ef96cdb6b156df3002f90323e0
Abdul Mateen <100369328+mateen470@users.noreply.github.com>
2023-03-31 19:28:57 +0500

    token checking middleware done

---
+++ server/middleware/middleware.js
@@ -0,0 +1,38 @@
+import jwt from "jsonwebtoken";
+
+const middleWare = {
+  tokenVerificationOfResetPasswordPage: async (req, res, next) => {
+    try {
+      const token = await req.headers("Authorization");
+
+      if (!token) {
+        return await res.status(400).json({
+          success: false,
+          message: "INVALID USER!!",
+        });
+      }
+
+      jwt.verify(
+        token,
+        process.env.ACCESS_TOKEN_SECRET_KEY,
+        async (error, user) => {
+          if (error) {
+            return await res.status(400).json({
+              success: false,
+              message: "INVALID TOKEN!!",
+            });
+          }
+          req.user = user;
+          next();
+        }
+      );
+    } catch (error) {
+      return await res.status(500).json({
+        success: false,
+        message: `PROCESS FAILED IN VERIFYING RESET PASSWORD PAGE TOKEN!!${error}`,
+      });
+    }
+  },
+};
+
+module.exports = middleWare;
