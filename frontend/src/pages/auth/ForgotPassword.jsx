fd10ebbe2b2408fc4ef954045933f5c7c4efe1db
Abdul Mateen <100369328+mateen470@users.noreply.github.com>
2023-05-03 18:33:16 +0500

    forgot password

--- frontend/src/pages/auth/ForgotPassword.jsx
+++ frontend/src/pages/auth/ForgotPassword.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-
+import React from "react";
+import ForgotPasswordForm from "../../components/forgotPassword-section/ForgotPasswordForm";
+import { Container } from "@mui/material";
 const ForgotPassword = () => {
   return (
-    <div>ForgotPassword</div>
-  )
-}
+    <Container
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <ForgotPasswordForm />
+    </Container>
+  );
+};
 
-export default ForgotPassword
+export default ForgotPassword;
