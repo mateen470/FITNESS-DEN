81496f9e1967cae66c95c74e5b0f130078fafce4
Abdul Mateen <100369328+mateen470@users.noreply.github.com>
2023-05-09 00:18:28 +0500

    .

--- frontend/src/components/trainer-dashboard-section/WorkoutPlanFormat.jsx
+++ frontend/src/components/trainer-dashboard-section/WorkoutPlanFormat.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { AddExcercise } from "../../context/Excercise";
 import { toast } from "react-toastify";
