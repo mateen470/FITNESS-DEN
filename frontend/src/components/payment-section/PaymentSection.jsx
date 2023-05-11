088b62264a79e58bdac8bad09878c60212137502
Abdul Mateen <100369328+mateen470@users.noreply.github.com>
2023-05-12 00:32:25 +0500

    .

--- frontend/src/components/payment-section/PaymentSection.jsx
+++ frontend/src/components/payment-section/PaymentSection.jsx
@@ -4,6 +4,8 @@ import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 import { AddPhysicalInfo } from "../../context/PhysicalInfo";
 import { SetSelectedPlanToBuy } from "../../context/SelectedPlan";
+import visa from "../../assets/visa.svg";
+import master from "../../assets/master.svg";
 import axios from "axios";
 import {
   CardNumberElement,
@@ -12,7 +14,7 @@ import {
   useStripe,
   useElements,
 } from "@stripe/react-stripe-js";
-import { Button, Container, FormControl, Box, Typography } from "@mui/material";
+import { Container, FormControl, Box, Typography } from "@mui/material";
 
 const PaymentSection = () => {
   const dispatch = useDispatch();
@@ -77,28 +79,124 @@ const PaymentSection = () => {
   return (
     <Container
       sx={{
+        my: 4,
         display: "flex",
         justifyContent: "center",
-        alignItems: "center",
+        minHeight: "100vh",
       }}
     >
-      <FormControl sx={{ gap: "2rem" }}>
-        <Box>
-          <Typography variant="subtitle1">Card Number</Typography>
-          <CardNumberElement />
-        </Box>
-        <Box>
-          <Typography variant="subtitle1">Card Expiry</Typography>
-          <CardExpiryElement />
-        </Box>
-        <Box>
-          <Typography variant="subtitle1">Card CVC</Typography>
-          <CardCvcElement />
-        </Box>
-        <Button variant="contained" onClick={handlePayment}>
-          Confirm Payment
-        </Button>
-      </FormControl>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h3" color={"white"} fontWeight={800}>
+          Enter Card Details
+        </Typography>
+        <FormControl sx={{ gap: "2rem", mt: 3 }}>
+          <Box>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <Typography
+                variant="h5"
+                color={"white"}
+                fontWeight={800}
+                fontFamily={"Comme, sans-serif"}
+              >
+                Card Number
+              </Typography>
+              <img src={visa} alt="visa-card" style={{ width: "6vw" }} />
+              <img src={master} alt="master-card" style={{ width: "5vw" }} />
+            </Box>
+            <Box
+              sx={{
+                borderBottom: "0.5px solid white",
+                minWidth: "100%",
+                mt: -2,
+              }}
+            >
+              <CardNumberElement />
+            </Box>
+          </Box>
+          <Box>
+            <Typography
+              variant="h5"
+              color={"white"}
+              fontWeight={800}
+              fontFamily={"Comme, sans-serif"}
+            >
+              Card Expiry
+            </Typography>
+            <Box sx={{ borderBottom: "0.5px solid white", minWidth: "100%" }}>
+              <CardExpiryElement />
+            </Box>
+          </Box>
+          <Box>
+            <Typography
+              variant="h5"
+              color={"white"}
+              fontWeight={800}
+              fontFamily={"Comme, sans-serif"}
+            >
+              Card CVC
+            </Typography>
+            <Box sx={{ borderBottom: "0.5px solid white", minWidth: "100%" }}>
+              <CardCvcElement />
+            </Box>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              mb: 4,
+              mt: -2,
+            }}
+          >
+            <Box
+              sx={{
+                border: "2px solid white",
+                p: 1,
+                px: 2,
+                mt: 3,
+                height: "3.6vw",
+                width: "8vw",
+                cursor: "pointer",
+              }}
+              onClick={handlePayment}
+            >
+              <Typography
+                color={"black"}
+                fontFamily={"Comme, sans-serif"}
+                sx={{
+                  background: "white",
+                  fontSize: "1.4vw",
+                  height: "3.4vw",
+                  width: "8.6vw",
+                  ml: -4.5,
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  transition: "scale 0.3s ease-in-out",
+                  fontWeight: "bold",
+                  "&:hover": {
+                    scale: "0.95 !important",
+                  },
+                }}
+              >
+                Submit
+              </Typography>
+            </Box>
+          </Box>
+        </FormControl>
+      </Box>
     </Container>
   );
 };
