7daac4652e016bfee28999d52bcc2604a8fb7105
Abdul Mateen <100369328+mateen470@users.noreply.github.com>
2023-06-04 22:58:39 +0500

    server shifted to another repo

--- server/model/allPayments.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const AllPaymentsSchema = mongoose.Schema({
-  PlanType: String,
-  PlanAmount: Number,
-});
-
-const AllPaymentsModel = mongoose.model("AllPayments", AllPaymentsSchema);
-module.exports = AllPaymentsModel;
--- server/model/updateDietPlan.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const updateDietPlan = new mongoose.Schema({
-  Description: String,
-  PlanID: String,
-});
-
-const updateDietPlanModel = new mongoose.model("UpdateDietPlan", updateDietPlan);
-module.exports = updateDietPlanModel;
